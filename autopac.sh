1adb7642bca9daf837195f4a428cd9be666faa93
solar <16302010038@fudan.edu.cn>
2018-11-16 17:12:20 +0800

    2018-11-16 update

--- autopac.sh
+++ autopac.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080"   --user-rule-from=user-rules.txt --pac-compress --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" --gfwlist-update-local -o autoproxy.pac
+genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080"   --user-rule-from=user-rules.txt --pac-compress --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" --gfwlist-update-local -o autoproxy.txt
 git add -A
 git commit -m "`date +"%F"` update"
-git push -u origin master
+git push -u sspac master
