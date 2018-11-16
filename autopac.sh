e13416d7189ec51f436193c52dc020044e0250ba
solar <16302010038@fudan.edu.cn>
2018-11-16 16:44:26 +0800

    20181611

--- autopac.sh
+++ autopac.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080" --pac-compress --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" --gfwlist-update-local -o autoproxy.pac
+genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080"   --user-rule-from=user-rules.txt --pac-compress --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" --gfwlist-update-local -o autoproxy.pac
 git add -A
 git commit -m "`date +"%Y%d%m"`"
-
+git push -u origin master
